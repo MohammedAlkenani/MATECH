c90e378471c0d1d3afb2316970dbf31d73ec7772
MohammedAlkenani <113029274+MohammedAlkenani@users.noreply.github.com>
2023-02-23 21:35:22 +0300

    Update GFG.java

--- GFG.java
+++ GFG.java
@@ -1,20 +1,20 @@
 import java.util.*;
-class GFG 
+class Home 
 {
 
-	public GFG() {
+	public Home() {
 	}
 	public static void main(String[] args)
 {
 	
-	Phone ph1=new Phone("IPhone 14 Pro","IOS 16 - 5G - 6.1-inch screen");
-	Phone ph2=new Phone("IPhone 13 Pro","iOS 15, upgradable to iOS 16.3 - 5g - 6.0-inch screen");
-	Phone ph3=new Phone("IPhone 12 Pro","IOS 16 - 5G - 6.1-inch screen");
-	Phone ph4=new Phone("IPhone 11 Pro","IOS 16 - 5G - 6.1-inch screen");
+	Phone ph1=new Phone("IPhone 14 Pro","IOS 16 - 5G - 6.1-inch screen" , 6000 );
+	Phone ph2=new Phone("IPhone 13 Pro","iOS 15, upgradable to iOS 16.3 - 5g - 6.0-inch screen" , 4000);
+	Phone ph3=new Phone("IPhone 12 Pro","IOS 16 - 5G - 6.1-inch screen" , 3900);
+	Phone ph4=new Phone("IPhone 11 Pro","IOS 16 - 5G - 6.1-inch screen" , 2700);
 
-	Laptop lab5=new Laptop("Asus TUF Gaming" , " IntelCore i5 -16GB -RAM512 GB -NVMe -M.2 -SSDWindows -11NVIDIA -GeForce 4 GB");
-	Laptop lab6=new Laptop("Mac Book ", "13.3 -M1 -8GB RAM -256GB SSD -macOS Big Sur -7 Core GPU");
-    Laptop lab7=new Laptop("Mac Book"  , " -13.3 -M1 -8GB RAM -256GB SSD -macOS Big Sur -7 Core GPU");
+	Laptop lab5=new Laptop("Asus TUF Gaming" , " IntelCore i5 -16GB -RAM512 GB -NVMe -M.2 -SSDWindows -11NVIDIA -GeForce 4 GB" , 3800);
+	Laptop lab6=new Laptop("MacBook Air "    , "13.3 -M1 -8GB RAM -256GB SSD -macOS Big Sur -7 Core GPU"                       , 5200);
+    Laptop lab7=new Laptop("MacBook Pro 14"  , " -14.2 -M1 Pro -16GB RAM -512GB -macOS Monterey -14Core GPU"                   , 7000);
 
 
 	Phone ph_list[]=new Phone[10];
@@ -30,7 +30,6 @@ class GFG
 	lab_list[2]=lab7;
 	printMenu(lab_list);
 }
-                                //print the menu             
 
 private static void printMenu(Phone[] ph_list) {
 	}
@@ -44,11 +43,12 @@ public static void printMenu(Phone[] ph_list, Laptop lab_list[])
 	{
 	int index=1;
 	try (Scanner sc = new Scanner(System.in)) {
-		System.out.println(" _____________ MATECH _____________ ");
+		System.out.println(" ___________________________________________________MATECH ___________________________________________________  ");
+		System.out.println(" _______________________ WELCOME TO MY SHOP _______________________ ");
 		System.out.println(" _____________ HOME _____________ /n ");
 		System.out.println(" _____________ Menu _____________ ");
-		System.out.println("1-Show Avilable Phones");
-		System.out.println("2-Add New Phone");
+		System.out.println("1-Show Avilable Phones and Laptops");
+		System.out.println("2-Add New Phone or Laptop");
 		System.out.println("3-Exit");
 
 			int a= sc.nextInt();
@@ -69,7 +69,7 @@ public static void printMenu(Phone[] ph_list, Laptop lab_list[])
 					ph_list[sp-1].getDetails();
 					lab_list[sp-1].PrintLaptopName(sp-1);
 					lab_list[sp-1].getdetailsLab();
-					System.out.println("Enter 2 to buy Device (Add to Cart) 4 to exit : ");
+					System.out.println("Enter 2 to buy Device (Add to Cart) -- 4 to exit : ");
 					int buy= sc.nextInt();
 					if(buy==2){
 						System.out.println("Enter Card Number");
@@ -79,14 +79,14 @@ public static void printMenu(Phone[] ph_list, Laptop lab_list[])
 					}
 				}
 				if(a==2){
-						System.out.println("Enter Phone Name");
-						String phName=sc.next();
-						System.out.println("Enter Phone Details");
-						String spec=sc.next();
+						System.out.println("Enter Device Name");
+						String Name=sc.next();
+						System.out.println("Enter Device Details");
+						String Details=sc.next();
 
-						Phone ph=new Phone(phName,spec);
+						Phone ph=new Phone(Name,Details, a);
 						ph_list[(index+1)]=ph;
-						System.out.println("Your Phone is Added Successfully");
+						System.out.println("Your Device is Added Successfully");
 						printMenu(ph_list , lab_list);
 			}
 	}
